9c703db2d56c3b4e1c1deaeffae04b473e77df88
Dustin Bracy <dustin@bracytechnology.com>
2025-04-11 03:04:39 +0000

    add ssh agent logic

--- postCreate.sh
+++ postCreate.sh
@@ -11,6 +11,19 @@ curl -o _git https://raw.githubusercontent.com/git/git/master/contrib/completion
 echo "zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash" >> .zshrc
 echo "fpath=(~/.zsh $fpath)" >> .zshrc
 echo "autoload -Uz compinit && compinit" >> .zshrc
+echo "eval \"$(ssh-agent -s)\" > /dev/null" >> .zshrc
+
+echo "if [ -z \"$SSH_AUTH_SOCK\" ]; then \
+   # Check for a currently running instance of the agent \
+   RUNNING_AGENT=\"`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`\" \ 
+   if [ \"$RUNNING_AGENT\" = \"0\" ]; then \
+        # Launch a new instance of the agent \
+        ssh-agent -s &> $HOME/.ssh/ssh-agent \
+   fi \
+   eval `cat $HOME/.ssh/ssh-agent` > /dev/null \
+ssh-add $HOME/.ssh/id_ed25519 2> /dev/null \
+fi" >> .zshrc
+
 
 #install sqlc
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
